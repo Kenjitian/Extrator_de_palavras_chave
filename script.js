1bc5a8e31a6fd02b0f178b87c04ff50d6b5db92c
Kenjitian <lucas.kenji.cardoso@escola.pr.gov.br>
2025-07-05 02:53:16 +0000

    Limitando até 10 palavras

--- script.js
+++ script.js
@@ -12,8 +12,19 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-     let frequencias = {};
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
     for (let i of palavras) {
         frequencias[i] = 0;
         for (let j of palavras) {
@@ -22,8 +33,5 @@ function processaTexto(texto) {
             }
         }
     }
-
-    console.log(frequencias);
-
-    return palavras;
+    return frequencias;
 }
