b5b8ea49d4f7c2107efc3f126bb45d90c61936aa
Kenjitian <lucas.kenji.cardoso@escola.pr.gov.br>
2025-07-09 01:33:13 +0000

    Finalizando trabalho com erro

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
+
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
@@ -17,7 +19,7 @@ function processaTexto(texto) {
         palavras[i] = palavras[i].toLowerCase();
     }
 
-     palavras = tiraPalavrasRuins(palavras);
+    palavras = tiraPalavrasRuins(palavras);
 
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
@@ -26,7 +28,6 @@ function processaTexto(texto) {
         return frequencias[p2] - frequencias[p1];
     }
 
-    console.log(ordenadas);
     return ordenadas.slice(0, 10);
 }
 
@@ -44,7 +45,6 @@ function contaFrequencias(palavras) {
 }
 
 function tiraPalavrasRuins(palavras) {
-    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
     const palavrasBoas = [];
     for (let palavra of palavras) {
         if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
